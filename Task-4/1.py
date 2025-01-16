94aae72a94ed2950915c6d59123c65e2cb3f1638
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-16 12:09:13 +0600

    1 2 3 4 5 6 7 8 9

--- Task-4/1.py
+++ Task-4/1.py
@@ -1,4 +1,12 @@
-for i  in range(1,9,3):
+n= int(input("Enter the number of n : "))
+for i  in range(1,n,3):
     for j in range(i,i+3):
         print(j, end=" ")
     print()
+    
+
+
+# Enter the number of n : 9
+# 1 2 3 
+# 4 5 6
+# 7 8 9
