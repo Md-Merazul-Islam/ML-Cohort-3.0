fd2c5377d7df055adbe3e7aa883c432377afc4fe
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-27 23:24:42 +0600

    Add summation function to calculate the sum of even numbers up to 100

--- week-3/task-1/1.py
+++ week-3/task-1/1.py
@@ -1,1 +1,11 @@
-print('Twinkle Twinkle Little Star')
+def summation():
+    n=1
+    sum = 0; 
+    for n in range(100):
+        if n%2 == 0:
+            sum += n
+            continue
+    return sum 
+add= summation()
+print("The summation is: ",add)
+আউটপুট:
